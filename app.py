fdba1d142c8ca72fc2766ce739157f063159f49e
Juan Mora <juanvalentin.mora@globant.com>
2021-11-27 22:42:38 -0300

    mejora del modelo

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers.core import Dense
+import matplotlib.pyplot as plt
 
 # cargamos las 4 combinaciones de las compuertas XOR
 training_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], "float32")
@@ -14,10 +15,18 @@ model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
 
-model.fit(training_data, target_data, epochs=1000)
+history = model.fit(training_data, target_data, epochs=1000)
 
+plt.figure()
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(history.history["loss"])
+plt.legend()
+
+plt.show()
+
+data_test = np.array([[1, 0], [1, 1], [0, 0], [0, 1]], "float32")
 # evaluamos el modelo
-scores = model.evaluate(training_data, target_data)
-
+scores = model.evaluate(data_test, target_data)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-print(model.predict(training_data).round())
+print(model.predict(data_test).round())
